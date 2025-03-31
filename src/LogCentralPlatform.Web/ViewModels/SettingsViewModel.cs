b5cf4253b9c842e46ac695be153a5de30701dc98
alonguetaptmed <alonguet@aptmed.fr>
2025-03-31 14:30:33 +0200

    Ajout des propriétés manquantes dans le modèle de Settings

--- src/LogCentralPlatform.Web/ViewModels/SettingsViewModel.cs
+++ src/LogCentralPlatform.Web/ViewModels/SettingsViewModel.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace LogCentralPlatform.Web.ViewModels
@@ -28,5 +30,51 @@ namespace LogCentralPlatform.Web.ViewModels
         [Display(Name = "Délai d'expiration de session (minutes)")]
         [Range(1, 1440, ErrorMessage = "Le délai doit être compris entre 1 et 1440 minutes")]
         public int SessionTimeoutMinutes { get; set; }
+        
+        // Propriétés supplémentaires requises par les vues
+        public string ApplicationName { get; set; }
+        public string ApplicationUrl { get; set; }
+        public string ApplicationVersion { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public int TotalServices { get; set; }
+        public int TotalLogs { get; set; }
+        public string AdminEmail { get; set; }
+        public string TimeZone { get; set; }
+        public int LogRetentionDays { get; set; }
+        public int MaxLogSizeMB { get; set; }
+        public bool MaintenanceModeEnabled { get; set; }
+        
+        // Classes imbriquées pour les paramètres avancés
+        public AISettingsModel AISettings { get; set; } = new AISettingsModel();
+        public NotificationSettingsModel NotificationSettings { get; set; } = new NotificationSettingsModel();
+    }
+    
+    public class AISettingsModel
+    {
+        public bool Enabled { get; set; }
+        public string ApiKey { get; set; }
+        public string Model { get; set; }
+        public int MaxTokens { get; set; }
+        public double Temperature { get; set; }
+        public int MaxResponseTime { get; set; }
+        public bool AutoAnalyzeErrors { get; set; }
+        public bool EnableCodeAccess { get; set; }
+    }
+    
+    public class NotificationSettingsModel
+    {
+        public bool EmailEnabled { get; set; }
+        public bool SlackEnabled { get; set; }
+        public bool WebhookEnabled { get; set; }
+        public string EmailRecipients { get; set; }
+        public string SlackWebhookUrl { get; set; }
+        public string CustomWebhookUrl { get; set; }
+        public string NotificationLevels { get; set; }
+        
+        // Options pour les notifications
+        public bool NotifyOnCritical { get; set; }
+        public bool NotifyOnError { get; set; }
+        public bool NotifyOnWarning { get; set; }
+        public bool NotifyOnServiceDown { get; set; }
     }
 }
